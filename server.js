4ae87b1a94ff850c703f87f5f80bdc3a08c284c2
abhimerni <braveb.abhishek@gmail.com>
2022-02-18 04:44:44 +0530

    git init

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const postRoute = require('./route/post.route')
+
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log("database is connectedd"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+//middleware 
+app.use(express.json())
+app.use('/api/post', postRoute)
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`app is running on port:${PORT}`);
+});
